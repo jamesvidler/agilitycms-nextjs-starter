cd6327d49b354c00630fe4ea9b9131b3fd11c048
azure_pipelines <support@agilitycms.com>
2021-07-23 15:18:08 -0400

    trying 302

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -29,7 +29,7 @@ export default async (req, res) => {
 	res.setPreviewData({})
 
 	// Redirect to the slug
-	res.writeHead(307, { Location: '/poop' })
+	res.writeHead(302, { Location: '/home' })
 	res.end()
 
 }
