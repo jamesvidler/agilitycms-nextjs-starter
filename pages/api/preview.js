7d687b745032a856ad5815aa7afbeb88458ac928
azure_pipelines <support@agilitycms.com>
2021-07-26 10:33:22 -0400

    updated qs on redirect

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -29,7 +29,7 @@ export default async (req, res) => {
 	res.setPreviewData({})
 
 	// Redirect to the slug
-	res.writeHead(307, { Location: `${previewUrl}?x=1` })
+	res.writeHead(307, { Location: `${previewUrl}?preview=1` })
 	res.end()
 
 }
