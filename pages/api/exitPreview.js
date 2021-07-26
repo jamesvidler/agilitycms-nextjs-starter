5a854be81225f3ce42b55643d92d31d4490b5835
azure_pipelines <support@agilitycms.com>
2021-07-26 10:39:40 -0400

    updated exit preview link

--- pages/api/exitPreview.js
+++ pages/api/exitPreview.js
@@ -4,6 +4,6 @@ export default async (req, res) => {
 	res.clearPreviewData()
 
 	// Redirect to the slug
-	res.writeHead(307, { Location: req.query.slug })
+	res.writeHead(307, { Location: `${req.query.slug}?preview=0` })
 	res.end()
 }
